f1057708a42e38b8fa1b11a6cc8a12547d25c7b4
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-06-06 20:28:34 +0530

    fileupload added

--- fileupload/app.py
+++ fileupload/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
+import uuid
 
 app = Flask(__name__)
 
@@ -6,7 +7,14 @@ app = Flask(__name__)
 @app.route("/upload", methods=["POST"])
 def upload_file():
     f = request.files["file"]
-    f.save(f"./uploads/{f.filename}")
+    filename = f"{uuid.uuid4()}.{f.filename.split('.')[-1]}"
+    f.save(f"./uploads/{filename}")
     return jsonify({
-        "message": "Uploaded to the destination"
+        "message": "Uploaded to the destination",
+        "filename": filename
     })
+
+
+@app.route("/file/<filename>")
+def read_file(filename):
+    return send_file(f"./uploads/{filename}")
