973895c709d71eaabb035cb7b87bcc6171865af2
Ritvik Shukla <ritvikshukla261@gmail.com>
2024-04-02 03:33:38 +0530

    structure creation

--- async_await/app.py
+++ async_await/app.py
@@ -1,7 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
 import aiohttp
 
+
 app = Flask(__name__)
+
+
+# Callback function for URL defaults for all view functions of the application.
+@app.url_defaults
+def add_language_code(endpoint, values):
+    # Check if "lang_code" is already in the URL values or if g.lang_code is  set.
+    if "lang_code" in values or not g.lang_code:
+        return  # Do nothing if lang_code is already in values or g.lang_code is  set.
+
+    # Check if the endpoint expects a "lang_code" parameter.
+    if app.url_map.is_endpoint_expecting(endpoint, "lang_code"):
+        # Set the "lang_code" value in the values dictionary to g.lang_code.
+        values["lang_code"] = g.lang_code
+
+
+# Callback function for URL value preprocessors for all view functions of the application.
+@app.url_value_preprocessor
+def pull_lang_code(endpoint, values):
+    # Pop the "lang_code" value from values dictionary and store it in g.lang_code.
+    g.lang_code = values.pop("lang_code", None)
 
 
 async def fetch_data():
